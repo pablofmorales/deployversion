850b6147dd7e6958433f40317960dd86c2769028
Pablo Morales <pablofmorales@gmail.com>
2015-11-12 17:41:10 -0300

    Fix Acceptance Test

--- tests/Acceptance/versions_specs.rb
+++ tests/Acceptance/versions_specs.rb
@@ -1,18 +1,10 @@
 require 'airborne'
 
-host = "http://localhost:8080"
+host = "http://deployversion:8000"
 
 describe 'Versions Resource' do
   it 'should return a json response with the latests version of testProject' do
-    get host + '/versions/latest/testProject'
-
-    expect_json({
-        :response => [
-            {
-                :project => 'testProject',
-                :version => '1.15.22'
-            }
-        ]
-    })
+    get host + '/versions/latest/test'
+    expect_json_keys('response', [:project, :version])
   end
 end
